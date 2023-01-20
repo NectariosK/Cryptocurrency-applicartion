360864590991161a748ae4852d8e74799e9bb71a
Nectarios Kisiigha <kisiighanectarios@gmail.com>
2023-01-20 22:47:45 +0100

    Initial Commit

---
+++ CryptocurrencyApp/CryptocurrencyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptocurrencyAppApp.swift
+//  CryptocurrencyApp
+//
+//  Created by Nectarios Kisiigha on 20/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct CryptocurrencyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
